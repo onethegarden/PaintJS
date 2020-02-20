0a92e406908d3c9c7f7217fe02f59ee3e79eb5bc
onethegarden <onethegarden@gmail.com>
2020-02-21 07:26:20 +0900

    range

--- paintJS/app.js
+++ paintJS/app.js
@@ -1,7 +1,8 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
-
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
 
 canvas.width=700;
 canvas.height=700;
@@ -10,6 +11,7 @@ ctx.strokeStyle="##2c2c2c";
 ctx.lineWidth=2.5;
 
 let painting = false;
+let filling = false;
 
 function stopPainting(){  
     painting=false;
@@ -37,6 +39,20 @@ function handleColorClick(event){
     ctx.strokeStyle = color;
     console.log(color);
 }
+
+function handleRangeChange(evnet){
+    const size = event.target.value;
+    ctx.lineWidth=size;
+}
+function handleModeClick(){
+    if(filling === true){
+        filling = false;
+        mode.innerText = "Fill";
+    }else{
+        filling=true;
+        mode.innerText = "painting";
+    }
+}
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
@@ -47,3 +63,10 @@ if(canvas){
 Array.from(colors).forEach(color => 
     color.addEventListener("click",handleColorClick)
 );
+
+if(range){
+    range.addEventListener("input", handleRangeChange)
+}
+if(mode){
+    mode.addEventListener("click", handleModeClick)
+}
