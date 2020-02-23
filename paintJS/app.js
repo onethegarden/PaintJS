96ab4199c8ea161d0150cbd87a4d936ae8a7456f
onethegarden <onethegarden@gmail.com>
2020-02-23 11:49:50 +0900

    save image, add README

--- paintJS/app.js
+++ paintJS/app.js
@@ -3,11 +3,20 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const save = document.getElementById("jsSave");
 
-canvas.width=700;
-canvas.height=700;
+const INITIAL_COLOR="#2c2c2c";
+const CANVAS_SIZE=700;
 
-ctx.strokeStyle="##2c2c2c";
+canvas.width=CANVAS_SIZE;
+canvas.height=CANVAS_SIZE;
+
+//set default color
+ctx.fillStyle="white";
+ctx.fillRect(0,0,CANVAS_SIZE,CANVAS_SIZE);
+
+ctx.strokeStyle=INITIAL_COLOR;
+ctx.fillStyle=INITIAL_COLOR;
 ctx.lineWidth=2.5;
 
 let painting = false;
@@ -37,7 +46,7 @@ function onMouseDown(event){
 function handleColorClick(event){
     const color= event.target.style.backgroundColor;
     ctx.strokeStyle = color;
-    console.log(color);
+    ctx.fillStyle = color;
 }
 
 function handleRangeChange(evnet){
@@ -53,11 +62,29 @@ function handleModeClick(){
         mode.innerText = "painting";
     }
 }
+function handleCanvasClick(){
+    if(filling){
+        ctx.fillRect(0,0,CANVAS_SIZE,CANVAS_SIZE);
+
+    }
+}
+function handleCM(event){
+    event.preventDefault();
+}
+function handleSaveClick(){
+    const image = canvas.toDataURL("image/png");
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PaintJS[ㅇ]";
+    link.click();
+}
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click",handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleCM);
 
 }
 Array.from(colors).forEach(color => 
@@ -65,8 +92,11 @@ Array.from(colors).forEach(color =>
 );
 
 if(range){
-    range.addEventListener("input", handleRangeChange)
+    range.addEventListener("input", handleRangeChange);
 }
 if(mode){
-    mode.addEventListener("click", handleModeClick)
+    mode.addEventListener("click", handleModeClick);
+}
+if(save){
+    save.addEventListener("click", handleSaveClick);
 }
